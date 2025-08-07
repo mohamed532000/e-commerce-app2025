3357ff14265ba908f3225630970a8e2b16a31e8b
Mohamed Ezat <ezat6518@gmail.com>
2025-08-08 01:43:35 +0300

    increase padding of add to cart and whishlist

--- components/ui/AddToCartBtn.jsx
+++ components/ui/AddToCartBtn.jsx
@@ -8,7 +8,7 @@ function AddToCartBtn({className}) {
   return (
     <button onClick={() => {
         console.log("Added Done")
-    }} className={`cursor-pointer rounded-xl py-2 px-3 bg-white dark:bg-background shadow-flexable-shadow flex justify-center items-center group ${className}`}>
+    }} className={`cursor-pointer rounded-xl py-3 px-4 bg-white dark:bg-background shadow-flexable-shadow flex justify-center items-center group ${className}`}>
         <CiShoppingCart className='text-2xl'/>
         <span className={`relative font-bold transition-all duration-300  opacity-0  ${currentLocale == "ar" ? "-mr-[50%]" : "-ml-[50%]"} group-hover:m-0 group-hover:opacity-100 text-foreground`}>AC</span>
     </button>
