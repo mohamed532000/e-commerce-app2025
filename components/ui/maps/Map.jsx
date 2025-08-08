80202ad152e35391f93ffebdc4e6e71b2f24ae3d
Mohamed Ezat <ezat6518@gmail.com>
2025-08-09 00:50:44 +0300

    add lazy loading to map

--- components/ui/maps/Map.jsx
+++ components/ui/maps/Map.jsx
@@ -4,7 +4,7 @@ function Map({className}) {
     const searchQuery = "haram ,Giza, Egypt";
     const mapUrl = `https://maps.google.com/maps?q=${encodeURIComponent(searchQuery)}&output=embed`;
     return (
-        <iframe className={`map ${className} rounded-3xl`} title="Map" src={mapUrl} width="100%" height="400px" ></iframe>
+        <iframe loading='lazy' className={`map ${className} rounded-3xl`} title="Map" src={mapUrl} width="100%" height="400px" ></iframe>
     )
 }
 
