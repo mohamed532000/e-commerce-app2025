2431fbf4afafdcfcc38d8074e60222c5d356035a
Mohamed Ezat <ezat6518@gmail.com>
2025-08-22 05:43:12 +0300

    sccs

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,26 +1,11 @@
 import RecommendedSection from "@/components/sections/home/RecommendedSection";
 import HeroSection from "../home-sections/HeroSection";
-import BestSaleSection from "@/components/sections/home/BestSaleSection";
-import { recommendedProducts } from "@/helper/fucntions/recommendedProducts";
-// import { bestSalePrducts } from "@/helper/fucntions/bestSaleProducts";
-import NewProductsSection from "@/components/sections/home/NewProductsSection";
-// import { newProducts } from "@/helper/fucntions/newProducts";
 
 export default async function Home() {
-  // const {data:recommendedData} = await recommendedProducts();
-  // const {data:newProductsData} =  await newProducts();
-  // const {data:bestSaleData} =  await bestSalePrducts();
-  // const [recommendedRes, bestSaleRes , newProductsRes] = await Promise.all([
-  //   recommendedProducts(),
-  //   bestSalePrducts(),
-  //   newProducts(),
-  // ]);
   return (
     <>
       <HeroSection/>
       <RecommendedSection products={[]}/>
-      {/* <NewProductsSection products={[]}/>
-      <BestSaleSection products={[]}/> */}
     </>
   );
 }
