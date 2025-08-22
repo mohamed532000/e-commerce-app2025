0c4752256c28664856372d215aebe9f2fdd8b032
Mohamed Ezat <ezat6518@gmail.com>
2025-08-22 05:47:11 +0300

    re add recommended section with products

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,11 +1,13 @@
 import RecommendedSection from "@/components/sections/home/RecommendedSection";
 import HeroSection from "../home-sections/HeroSection";
+import { recommendedProducts } from "@/helper/fucntions/recommendedProducts";
 
 export default async function Home() {
+  const {data:recommendedData} = await recommendedProducts();
   return (
     <>
       <HeroSection/>
-      <RecommendedSection products={[]}/>
+      <RecommendedSection products={recommendedData}/>
     </>
   );
 }
