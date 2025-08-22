6c192b78b061d1fe09bfd5ba80d1c89e2007a574
Mohamed Ezat <ezat6518@gmail.com>
2025-08-22 04:50:23 +0300

    disable fetch best sale and new products requests

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,14 +2,14 @@ import RecommendedSection from "@/components/sections/home/RecommendedSection";
 import HeroSection from "../home-sections/HeroSection";
 import BestSaleSection from "@/components/sections/home/BestSaleSection";
 import { recommendedProducts } from "@/helper/fucntions/recommendedProducts";
-import { bestSalePrducts } from "@/helper/fucntions/bestSaleProducts";
+// import { bestSalePrducts } from "@/helper/fucntions/bestSaleProducts";
 import NewProductsSection from "@/components/sections/home/NewProductsSection";
-import { newProducts } from "@/helper/fucntions/newProducts";
+// import { newProducts } from "@/helper/fucntions/newProducts";
 
 export default async function Home() {
   const {data:recommendedData} = await recommendedProducts();
-  const {data:newProductsData} =  await newProducts();
-  const {data:bestSaleData} =  await bestSalePrducts();
+  // const {data:newProductsData} =  await newProducts();
+  // const {data:bestSaleData} =  await bestSalePrducts();
   // const [recommendedRes, bestSaleRes , newProductsRes] = await Promise.all([
   //   recommendedProducts(),
   //   bestSalePrducts(),
@@ -19,8 +19,8 @@ export default async function Home() {
     <>
       <HeroSection/>
       <RecommendedSection products={recommendedData}/>
-      <NewProductsSection products={newProductsData}/>
-      <BestSaleSection products={bestSaleData}/>
+      <NewProductsSection products={[]}/>
+      <BestSaleSection products={[]}/>
     </>
   );
 }
