28e0339515997bce6f8ce2ce4d45922b147c73d8
Mohamed Ezat <ezat6518@gmail.com>
2025-09-09 04:09:14 +0300

    update in pagination

--- components/ui/shop-page-components/ShopPagination.jsx
+++ components/ui/shop-page-components/ShopPagination.jsx
@@ -34,12 +34,12 @@ function ShopPagination({pagesCount , handleUpdatePageNum , loadingProducts}) {
                    (
                     currentLocale == "ar"
                     ?
-                    <MdKeyboardDoubleArrowRight className='m-0 p-0'/> 
+                    <MdKeyboardDoubleArrowRight className='m-0 p-0' aria-hidden="true"/> 
                     :
-                    <MdKeyboardDoubleArrowLeft/> 
+                    <MdKeyboardDoubleArrowLeft aria-hidden="true"/> 
                     )
                 }
-                <span aria-hidden="true" className={`${slicePagination.from > 0 ? "-translate-y-[1px]" : ""}`}>{globalT("Prev")}</span>
+                <span className={`${slicePagination.from > 0 ? "-translate-y-[1px]" : ""}`}>{globalT("Prev")}</span>
             </Button>
             {Array.from({ length: pagesCount }, (_, i) => i + 1) // [1, 2, 3, ...]
             .slice(slicePagination.from, slicePagination.to)
@@ -49,16 +49,16 @@ function ShopPagination({pagesCount , handleUpdatePageNum , loadingProducts}) {
             <Button 
             disabled={slicePagination.to == pagesCount || loadingProducts}
             className={`cursor-pointer ${(slicePagination.to == pagesCount || loadingProducts) ? "pointer-events-none opacity-80" : ""}`} onClick={() => handleIncrementSlice()}>
-                <span aria-hidden="" className={`${slicePagination.to < pagesCount ? "-translate-y-[1px]" : ""}`}>{globalT("Next")}</span>
+                <span className={`${slicePagination.to < pagesCount ? "-translate-y-[1px]" : ""}`}>{globalT("Next")}</span>
                 {
                    slicePagination.to < pagesCount
                    &&
                    (
                     currentLocale == "ar"
                     ?
-                    <MdKeyboardDoubleArrowLeft/> 
+                    <MdKeyboardDoubleArrowLeft aria-hidden="true"/> 
                     :
-                    <MdKeyboardDoubleArrowRight className='m-0 p-0'/> 
+                    <MdKeyboardDoubleArrowRight className='m-0 p-0' aria-hidden="true"/> 
                     )
                 }
             </Button>
