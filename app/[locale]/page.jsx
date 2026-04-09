14e0312e3e852e1b2219691417327d436f2c925a
Mohamed Ezat <ezat6518@gmail.com>
2026-04-10 01:25:54 +0200

    un commented recommended & best sales & new items sections in home page and commented SmootherProvider in layout

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,18 +1,18 @@
 import HeroSection from "@/components/sections/home/hero-section/HeroSection";
 import axios from "axios";
 import SetCookies from "@/helper/SetCookies";
-// import RecommendedItems from "@/components/sections/home/recommended/RecommendedItems";
-// import BestSalesitems from "@/components/sections/home/best-sales/BestSalesitems";
-// import NewItemsSection from "@/components/sections/home/new-items/NewItemsSection";
+import RecommendedItems from "@/components/sections/home/recommended/RecommendedItems";
+import BestSalesitems from "@/components/sections/home/best-sales/BestSalesitems";
+import NewItemsSection from "@/components/sections/home/new-items/NewItemsSection";
 export default async function Home({params}) {
   const {locale} = await params;
   const settings = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/settings`);
   return (
     <>
       <HeroSection/>
-      {/* <RecommendedItems locale={locale}/>
+      <RecommendedItems locale={locale}/>
       <BestSalesitems locale={locale} /> 
-      <NewItemsSection locale={locale} />  */}
+      <NewItemsSection locale={locale} /> 
       <SetCookies/>
     </>
   );
