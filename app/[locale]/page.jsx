1e08db58b25f8c97ae1a3cf5bbeb9c4a286e5b14
Mohamed Ezat <ezat6518@gmail.com>
2026-04-11 06:36:16 +0200

    add current locale to settings request in home page

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,12 +1,11 @@
 import HeroSection from "@/components/sections/home/hero-section/HeroSection";
-// import axios from "axios";
 import SetCookies from "@/helper/SetCookies";
 import RecommendedItems from "@/components/sections/home/recommended/RecommendedItems";
 import BestSalesitems from "@/components/sections/home/best-sales/BestSalesitems";
 import NewItemsSection from "@/components/sections/home/new-items/NewItemsSection";
 export default async function Home({params}) {
   const {locale} = await params;
-  const settings = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/settings`);
+  const settings = await axios.get(`${process.env.NEXT_PUBLIC_URL}/${locale}/api/settings`);
   return (
     <>
       <HeroSection/>
--- helper/SetCookies.jsx
+++ helper/SetCookies.jsx
@@ -1,11 +1,14 @@
 "use client"
 import axios from 'axios';
+import { useLocale } from 'next-intl';
 import { useEffect } from 'react'
 
 function SetCookies() {
+    const currentLocale = useLocale();
     useEffect(() => {
         // used this func to set cookies on the server with calling route handler
         const callSetRoute = async () => {
+            // const settings = await axios.get(`${process.env.NEXT_PUBLIC_URL}/${currentLocale}/api/settings`);
             const settings = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/settings`);
         }
         callSetRoute()
