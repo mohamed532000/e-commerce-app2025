609ec830bddd0a9856524f09242665c7d1893061
Mohamed Ezat <ezat6518@gmail.com>
2026-04-11 20:43:28 +0200

    remove settings request from home page

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -3,10 +3,8 @@ import SetCookies from "@/helper/SetCookies";
 import RecommendedItems from "@/components/sections/home/recommended/RecommendedItems";
 import BestSalesitems from "@/components/sections/home/best-sales/BestSalesitems";
 import NewItemsSection from "@/components/sections/home/new-items/NewItemsSection";
-import axios from "axios";
 export default async function Home({params}) {
   const {locale} = await params;
-  const settings = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/settings`).then((res) => console.log(res));
   return (
     <>
       <HeroSection/>
