cb7e2b58380a4115166ff237bebb753d4c5483dc
Mohamed Ezat <ezat6518@gmail.com>
2026-05-03 02:09:21 +0300

    update above footer boxes border for small screens and miduem

--- components/ui/AboveFooter.jsx
+++ components/ui/AboveFooter.jsx
@@ -7,25 +7,26 @@ import { Link } from '@/i18n/navigation';
 
 async function AboveFooter() {
     const t = await getTranslations("aboveFooter");
+    const boxBorder = "border-b-[.3px] md:border-b-0 md:ltr:border-r-[.3px] md:rtl:border-l-[.3px] border-slate-200";
     return (
         <div className="border-y-[.3px] border-slate-200 dark:bg-transparent mt-[50px]">
-            <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 divide-y sm:divide-y-0 lg:divide-x dark:divide-slate-800 divide-slate-200 rtl:divide-x-reverse">
+            <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ">
                 {/* Item 1 */}
-                <div className="flex flex-col items-center justify-center text-center py-10 px-4">
+                <div className={`flex flex-col items-center justify-center text-center py-10 px-4 ${boxBorder}`}>
                     <CiDeliveryTruck className="text-[40px] mb-4 text-slate-800 dark:text-slate-200" />
                     <h3 className="font-serif text-lg mb-2 text-slate-900 dark:text-slate-100">{t("Free Shipping")}</h3>
                     <p className="text-xs">{t("Fast & free delivery")}</p>
                 </div>
 
                 {/* Item 2 */}
-                <div className="flex flex-col items-center justify-center text-center py-10 px-4 ltr:border-l-[.3px] rtl:border-r-[.3px] border-slate-200">
+                <div className={`flex flex-col items-center justify-center text-center py-10 px-4 ${boxBorder}`}>
                     <TbTruckReturn className="text-[40px] mb-4 text-slate-800 dark:text-slate-200" />
                     <h3 className="font-serif text-lg mb-2 text-slate-900 dark:text-slate-100">{t("90 Day Returns")}</h3>
                     <p className="text-xs">{t("Easy returns & exchanges")}</p>
                 </div>
 
                 {/* Item 3 */}
-                <div className="flex flex-col items-center justify-center text-center py-10 px-4 ltr:border-r-[.3px] rtl:border-l-[.3px] border-slate-200">
+                <div className={`flex flex-col items-center justify-center text-center py-10 px-4 ${boxBorder}`}>
                     <CiStar className="text-[40px] mb-4 text-slate-800 dark:text-slate-200" />
                     <h3 className="font-serif text-lg mb-2 text-slate-900 dark:text-slate-100">{t("5-Star Care")}</h3>
                     <p className="text-xs">
